772246b1ecf1819bb0b3bc1e09903ce88b826903
PrLamkin <shiektheimmortal@outlook.com>
2020-11-04 11:35:43 -0600

    location set up

--- views/Angular.js
+++ views/Angular.js
@@ -53,3 +53,45 @@ function checkTime(i) {
     if (i < 10) { i = "0" + i };
     return i;
 }
+
+var x = document.getElementById("coordinate");
+var y = document.getElementById("json");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML =
+        "Latitude: " +
+        position.coords.latitude +
+        "<br>Longitude: " +
+        position.coords.longitude;
+
+    //Create query for the API.
+    var latitude = "latitude=" + position.coords.latitude;
+    var longitude = "&longitude=" + position.coords.longitude;
+    var query = latitude + longitude + "&localityLanguage=en";
+
+    const Http = new XMLHttpRequest();
+
+    var bigdatacloud_api =
+        "https://api.bigdatacloud.net/data/reverse-geocode-client?";
+
+    bigdatacloud_api += query;
+
+    Http.open("GET", bigdatacloud_api);
+    Http.send();
+
+    Http.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = JSON.parse(this.responseText);
+            console.log(myObj);
+            y.innerHTML += myObj.locality + ", " + myObj.principalSubdivision;
+        }
+    };
+}
