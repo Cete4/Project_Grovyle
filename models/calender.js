6825485ee5d507d5bbe69b81bbc634d68942ea5d
violettaval <violetvalencia37@gmail.com>
2020-11-04 12:53:51 -0600

    models new names

--- models/calender.js
+++ models/calender.js
@@ -1,6 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
     var Calendar= sequelize.define("Calendar", {
-
         DATE: DataTypes.DATETIME,
         LOCATION: DataTypes.STRING,
         TEMP: DataTypes.INTEGER, 
